92088503e74f623d77e65f187d2e0073bfffe29e
António Santos <antoniojps@ua.pt>
2019-02-12 01:20:53 +0000

    Rework create-react-app to create-react-library

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,323 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import js from 'react-syntax-highlighter/dist/languages/hljs/javascript'
+import monokai from 'react-syntax-highlighter/dist/styles/hljs/monokai'
+import Minimap from 'react-simple-minimap'
 
-import ExampleComponent from 'react-simple-minimap'
+SyntaxHighlighter.registerLanguage('javascript', js)
 
-export default class App extends Component {
-  render () {
+class App extends Component {
+  renderCode = () => (
+    <Code>
+      <SyntaxHighlighter
+        styles={{ marginTop: '-1rem' }}
+        language="js"
+        style={monokai}
+        customStyle={{ backgroundColor: '#292A3A' }}
+      >
+        {`
+  import React, { Component } from 'react'
+  import styled from 'styled-components'
+  import PropTypes from 'prop-types'
+
+  export default class MiniMap extends Component {
+    constructor(props) {
+      super(props)
+      this.refMinimap = React.createRef()
+      this.state = {
+        scrollPercent: 0,
+        windowScroll: 0,
+        windowWidth: document.documentElement.clientWidth
+      }
+    }
+
+    componentDidMount() {
+      window.addEventListener('scroll', this.updateScrollPercent)
+      window.addEventListener('resize', this.updateWidth)
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('scroll', this.updateScrollPercent)
+      window.removeEventListener('resize', this.updateWidth)
+    }
+
+    updateWidth = () => {
+      this.setState(() => ({
+        windowWidth: document.documentElement.clientWidth
+      }))
+    }
+
+    updateScrollPercent = () => {
+      const winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop
+
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight
+
+      const scrolled = winScroll / height
+      this.scrollToPercent()
+      this.setState(() => ({
+        scrollPercent: scrolled
+      }))
+    }
+
+    scrollToPercent = () => {
+      const { scrollPercent } = this.state
+      const minimap = this.refMinimap.current
+
+      const minimapHeight = minimap.clientHeight
+      const scrollHeight = minimap.scrollHeight
+
+      const height = scrollHeight - minimapHeight
+
+      minimap.scrollTo(0, height * scrollPercent)
+    }
+
+    computeScale = () => {
+      const { windowWidth } = this.state
+      const minimapWidth = 114
+      const scale = minimapWidth / windowWidth
+      return scale
+    }
+
+    render() {
+      const { of } = this.props
+      const scale = this.computeScale()
+      return (
+        <MinimapWindow ref={this.refMinimap}>
+          <Preview scale={scale}>{of}</Preview>
+        </MinimapWindow>
+      )
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // damn, you're actually reading this?
+  // so what are you waiting for?
+  // yarn add react-simple-minimap
+
+        `}
+      </SyntaxHighlighter>
+    </Code>
+  )
+
+  renderPage = () => (
+    <div className="App">
+      <Header>
+        <h1>React Simple Minimap</h1>
+        <p>
+          Look to your right
+          <br />
+          <span role="img" aria-label="RightPointer">
+            👉
+          </span>
+        </p>
+      </Header>
+      {this.renderCode()}
+    </div>
+  )
+
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <Fragment>
+        <Minimap of={this.renderPage()} />
+        {this.renderPage()}
+      </Fragment>
     )
   }
 }
+
+const Code = styled.div`
+  margin-top: -1rem;
+`
+
+const Header = styled.div`
+  color: #ebf2f1;
+  background-color: #1e202b;
+  padding: 1rem;
+  h1 {
+    font-size: 2rem;
+    margin-top: 0;
+  }
+  p {
+    font-size: 1rem;
+  }
+`
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,20 +3,82 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+class Minimap extends Component {
+  constructor(props) {
+    super(props)
+    this.refMinimap = React.createRef()
+    this.state = {
+      scrollPercent: 0,
+      windowScroll: 0,
+      windowWidth: document.documentElement.clientWidth
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.updateScrollPercent)
+    window.addEventListener('resize', this.updateWidth)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.updateScrollPercent)
+    window.removeEventListener('resize', this.updateWidth)
+  }
+
+  updateWidth = () => {
+    this.setState(() => ({
+      windowWidth: document.documentElement.clientWidth
+    }))
+  }
+
+  updateScrollPercent = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    const scrolled = winScroll / height
+    this.scrollToPercent()
+    this.setState(() => ({
+      scrollPercent: scrolled
+    }))
+  }
+
+  scrollToPercent = () => {
+    const { scrollPercent } = this.state
+    const minimap = this.refMinimap.current
+
+    const minimapHeight = minimap.clientHeight
+    const scrollHeight = minimap.scrollHeight
+
+    const height = scrollHeight - minimapHeight
+
+    minimap.scrollTo(0, height * scrollPercent)
+  }
+
+  computeScale = () => {
+    const { windowWidth } = this.state
+    const minimapWidth = 114
+    const scale = minimapWidth / windowWidth
+    return { transform: `scale(${scale || 1})` }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
+    const { of } = this.props
+    const scale = this.computeScale()
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.minimapWindow} ref={this.refMinimap}>
+        <div className={styles.minimapWindow__preview} style={scale}>
+          {of}
+        </div>
       </div>
     )
   }
 }
+
+Minimap.propTypes = {
+  of: PropTypes.node.isRequired
+}
+
+export default Minimap
